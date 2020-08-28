ece9c1432959fc31b9b0c7aea4c6a53f821bc954
Jeff Bauerle <jeffbauerle@Jeffs-MacBook-Pro.local>
2020-08-28 06:00:02 -0600

    cleaning

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -100,14 +100,6 @@ def plot_10_most_common_words(count_data, count_vectorizer):
     # plt.show()
 
 
-    def print_topics(model, count_vectorizer, n_top_words):
-        words = count_vectorizer.get_feature_names()
-        for topic_idx, topic in enumerate(model.components_):
-            print("\nTopic #%d:" % topic_idx)
-            print(" ".join([words[i]
-                            for i in topic.argsort()[:-n_top_words - 1:-1]]))
-
-
 if __name__ == "__main__":
 
     stopwords = set(STOPWORDS)
