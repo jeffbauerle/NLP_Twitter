0cd306a54aa2b99cd762cbbeffad1dbc97941269
Jeff Bauerle <jeffbauerle@Jeffs-MacBook-Pro.local>
2020-08-28 03:32:03 -0600

    moved function

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -77,6 +77,26 @@ def vectorize(df, column, stop_words):
     return X, features
 
 
+def plot_10_most_common_words(count_data, count_vectorizer):
+    words = count_vectorizer.get_feature_names()
+    total_counts = np.zeros(len(words))
+    for t in count_data:
+        total_counts += t.toarray()[0]
+    count_dict = (zip(words, total_counts))
+    count_dict = sorted(count_dict, key=lambda x: x[1], reverse=True)[0:10]
+    words = [w[0] for w in count_dict]
+    counts = [w[1] for w in count_dict]
+    x_pos = np.arange(len(words))
+    plt.figure(2, figsize=(15, 15/1.6180))
+    plt.subplot(title='10 most common words')
+    sns.set_context("notebook", font_scale=1.25, rc={"lines.linewidth": 2.5})
+    sns.barplot(x_pos, counts, palette='husl')
+    plt.xticks(x_pos, words, rotation=90)
+    plt.xlabel('words')
+    plt.ylabel('counts')
+    plt.show()
+
+
 if __name__ == "__main__":
 
     stopwords = set(STOPWORDS)
@@ -112,126 +132,93 @@ if __name__ == "__main__":
 
     all_df = all_df[english_df]
     print(all_df.head())
-# directory = '../data'
-# # df_namer = "df"
-# counter = 1
+    # directory = '../data'
+    # # df_namer = "df"
+    # counter = 1
 
-# print(my_dict)
+    # print(my_dict)
 
-# Commented out
-# TF IDF
-v = TfidfVectorizer()  # max_features = 30000
-X = v.fit_transform(all_df['text'])
+    # Commented out
+    # TF IDF
+    v = TfidfVectorizer()  # max_features = 30000
+    X = v.fit_transform(all_df['text'])
+
+    # Load the library with the CountVectorizer method
+
+    sns.set_style('whitegrid')
+    # Helper function
+
+    # Initialise the count vectorizer with the English stop words
+    count_vectorizer = CountVectorizer(stop_words=stopwords)
+    # Fit and transform the processed titles
+    count_data = count_vectorizer.fit_transform(all_df['text'])
+    # Visualise the 10 most common words
+    plot_10_most_common_words(count_data, count_vectorizer)
 
 
-# Load the library with the CountVectorizer method
+    # Load the LDA model from sk-learn
 
-sns.set_style('whitegrid')
-# Helper function
+    # Helper function
+    def print_topics(model, count_vectorizer, n_top_words):
+        words = count_vectorizer.get_feature_names()
+        for topic_idx, topic in enumerate(model.components_):
+            print("\nTopic #%d:" % topic_idx)
+            print(" ".join([words[i]
+                            for i in topic.argsort()[:-n_top_words - 1:-1]]))
 
 
-def plot_10_most_common_words(count_data, count_vectorizer):
-    import matplotlib.pyplot as plt
-    words = count_vectorizer.get_feature_names()
-    total_counts = np.zeros(len(words))
-    for t in count_data:
-        total_counts += t.toarray()[0]
-    count_dict = (zip(words, total_counts))
-    count_dict = sorted(count_dict, key=lambda x: x[1], reverse=True)[0:10]
-    words = [w[0] for w in count_dict]
-    counts = [w[1] for w in count_dict]
-    x_pos = np.arange(len(words))
-    plt.figure(2, figsize=(15, 15/1.6180))
-    plt.subplot(title='10 most common words')
-    sns.set_context("notebook", font_scale=1.25, rc={"lines.linewidth": 2.5})
-    sns.barplot(x_pos, counts, palette='husl')
-    plt.xticks(x_pos, words, rotation=90)
-    plt.xlabel('words')
-    plt.ylabel('counts')
+    text = " ".join(tweet for tweet in all_df.text)
+    print("There are {} words in the combination of all review.".format(len(text)))
+
+    wordcloud = WordCloud(stopwords=stopwords).generate(text)
+
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.tight_layout()
     plt.show()
-# Initialise the count vectorizer with the English stop words
-count_vectorizer = CountVectorizer(stop_words=stopwords)
-# Fit and transform the processed titles
-count_data = count_vectorizer.fit_transform(all_df['text'])
-# Visualise the 10 most common words
-plot_10_most_common_words(count_data, count_vectorizer)
+
+    #  Coronavirus, Equality/Diversity, climate/enviromental,
+    # business/finance, and other as a catachall
+
+    vocab = count_vectorizer.get_feature_names()
+    word2id = dict((v, idx) for idx, v in enumerate(vocab))
+    print(vocab)
 
 
-# Load the LDA model from sk-learn
-
-# Helper function
-def print_topics(model, count_vectorizer, n_top_words):
-    words = count_vectorizer.get_feature_names()
-    for topic_idx, topic in enumerate(model.components_):
-        print("\nTopic #%d:" % topic_idx)
-        print(" ".join([words[i]
-                        for i in topic.argsort()[:-n_top_words - 1:-1]]))
-
-# Commented out old model
-# # Tweak the two parameters below
-# number_topics = 5
-# number_words = 10
-# # Create and fit the LDA model
-# lda = LDA(n_components=number_topics, n_jobs=-1)
-# lda.fit(count_data)
-# # Print the topics found by the LDA model
-# print("Topics found via LDA:")
-# print_topics(lda, count_vectorizer, number_words)
-
-text = " ".join(tweet for tweet in all_df.text)
-print("There are {} words in the combination of all review.".format(len(text)))
+    seed_topic_list = [['coronavirus', 'covid', 'covid-19', 'virus', 'curve',
+                        'flat', 'coronavirusoutbreak', 'corona'],
+                    ['equality', 'diversity', 'china', 'equalitywarrior',
+                    'equally', 'equal', 'diversityandinclusion',
+                        'diverse', 'chinese', 'uyghur', 'forceduyghurlabor',
+                        'forced', 'forcedlabour', 'forcedlabor',
+                        'forcedchildlabor', 'forcedslave'],
+                    ['climate', 'environmental', 'climatechange', 'fuel',
+                    'fossil', 'renewable', 'carbon'],
+                    ['business', 'finance', 'economic', 'investment', 'money',
+                    'bank', 'stocks', 'stockstobuy', 'stockstowatch',
+                        'stockmarkets'],
+                    ['fashion', 'poshmarkapp', 'poshmark', 'style']]
+    model = guidedlda.GuidedLDA(n_topics=7, n_iter=100, random_state=7, refresh=20)
 
 
-wordcloud = WordCloud(stopwords=stopwords).generate(text)
+    seed_topics = {}
+    for t_id, st in enumerate(seed_topic_list):
+        for word in st:
+            if word in word2id:
+                seed_topics[word2id[word]] = t_id
 
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.tight_layout()
-plt.show()
+    model.fit(count_data, seed_topics=seed_topics, seed_confidence=0.95)
 
-#  Coronavirus, Equality/Diversity, climate/enviromental,
-# business/finance, and other as a catachall
+    topic_word = model.topic_word_
+    n_top_words = 20
+    topics = ['coronavirus', 'equality/diversity', 'climate',
+            'business/finance', 'other', 'other2', 'other3']
+    for i, topic_dist in enumerate(topic_word):
+        topic_words = np.array(vocab)[np.argsort(topic_dist)][:-(n_top_words+1):-1]
+        print(topics[i])
+        print('Topic {}: {}'.format(i, ' '.join(topic_words)))
 
-
-vocab = count_vectorizer.get_feature_names()
-word2id = dict((v, idx) for idx, v in enumerate(vocab))
-print(vocab)
-
-
-seed_topic_list = [['coronavirus', 'covid', 'covid-19', 'virus', 'curve',
-                    'flat', 'coronavirusoutbreak', 'corona'],
-                   ['equality', 'diversity', 'china', 'equalitywarrior',
-                   'equally', 'equal', 'diversityandinclusion',
-                    'diverse', 'chinese', 'uyghur', 'forceduyghurlabor',
-                    'forced', 'forcedlabour', 'forcedlabor',
-                    'forcedchildlabor', 'forcedslave'],
-                   ['climate', 'environmental', 'climatechange', 'fuel',
-                   'fossil', 'renewable', 'carbon'],
-                   ['business', 'finance', 'economic', 'investment', 'money',
-                   'bank', 'stocks', 'stockstobuy', 'stockstowatch',
-                    'stockmarkets'],
-                   ['fashion', 'poshmarkapp', 'poshmark', 'style']]
-model = guidedlda.GuidedLDA(n_topics=7, n_iter=100, random_state=7, refresh=20)
-
-
-seed_topics = {}
-for t_id, st in enumerate(seed_topic_list):
-    for word in st:
-        if word in word2id:
-            seed_topics[word2id[word]] = t_id
-
-model.fit(count_data, seed_topics=seed_topics, seed_confidence=0.95)
-
-topic_word = model.topic_word_
-n_top_words = 20
-topics = ['coronavirus', 'equality/diversity', 'climate',
-          'business/finance', 'other', 'other2', 'other3']
-for i, topic_dist in enumerate(topic_word):
-    topic_words = np.array(vocab)[np.argsort(topic_dist)][:-(n_top_words+1):-1]
-    print(topics[i])
-    print('Topic {}: {}'.format(i, ' '.join(topic_words)))
-
-doc_topic = model.transform(count_data)
-for i in range(9):
-    print("top topic: {} Document: {}".format(doc_topic[i].argmax(),
-                                              ', '.join(np.array(vocab)[count_data[i, :].toarray().argsort()][0][::-1][0:10])))
+    doc_topic = model.transform(count_data)
+    for i in range(9):
+        print("top topic: {} Document: {}".format(doc_topic[i].argmax(),
+                                                ', '.join(np.array(vocab)[count_data[i, :].toarray().argsort()][0][::-1][0:10])))
